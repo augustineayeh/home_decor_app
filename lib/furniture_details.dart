476c375d3af2ff98a65397713e82af3a67e37e26
Augustine <augyog07@gmail.com>
2023-04-11 16:18:12 +0000

    Modified the details page and added more readme files

--- lib/furniture_details.dart
+++ lib/furniture_details.dart
@@ -57,9 +57,9 @@ class FurnitureDetails extends StatelessWidget {
             const SizedBox(
               child: Padding(
                 padding: EdgeInsets.only(
-                  top: 10,
-                  left: 10,
-                  right: 10,
+                  top: 15,
+                  left: 25,
+                  right: 25,
                 ),
                 child: Text(
                   'Cozy up in this plush, oversized bed - perfect for lazy Sunday mornings and late-night reading sessions.',
@@ -71,7 +71,7 @@ class FurnitureDetails extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(25),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: const [
@@ -88,8 +88,8 @@ class FurnitureDetails extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.only(
-                left: 10,
-                right: 10,
+                left: 25,
+                right: 25,
               ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -127,8 +127,8 @@ class FurnitureDetails extends StatelessWidget {
               children: [
                 const Padding(
                   padding: EdgeInsets.only(
-                    left: 10,
-                    right: 10,
+                    left: 25,
+                    right: 25,
                   ),
                   child: Text(
                     'Sizes:',
@@ -139,8 +139,7 @@ class FurnitureDetails extends StatelessWidget {
                   width: 200,
                   child: Padding(
                     padding: const EdgeInsets.only(
-                      left: 10,
-                      right: 10,
+                      right: 25,
                     ),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -204,33 +203,31 @@ class FurnitureDetails extends StatelessWidget {
               ],
             ),
             const SizedBox(
-              height: 20,
+              height: 30,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Padding(
                   padding: const EdgeInsets.only(
-                    left: 10,
-                    right: 10,
+                    left: 25,
+                    right: 25,
                   ),
                   child: Container(
-                    alignment: Alignment.centerRight,
-                    child: IconButton(
-                      iconSize: 40.0,
-                      icon: const Icon(Icons.add_shopping_cart),
-                      onPressed: () {},
-                    ),
-                  ),
+                      alignment: Alignment.centerRight,
+                      child: Image.asset(
+                        'assets/images/shopping-cart-2.png',
+                        height: 40,
+                      )),
                 ),
                 Padding(
                   padding: const EdgeInsets.only(
-                    left: 10,
-                    right: 10,
+                    left: 25,
+                    right: 25,
                   ),
                   child: SizedBox(
                     height: 40,
-                    width: 250,
+                    width: 210,
                     child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.black,
@@ -263,76 +260,82 @@ class FurnitureDetails extends StatelessWidget {
               child: Text(
                 '   Shop similar items:',
                 style: TextStyle(
-                  fontSize: 20,
+                  fontSize: 22,
                 ),
               ),
             ),
-            SizedBox(
-              height: 100,
-              width: double.infinity,
-              child: MediaQuery.removePadding(
-                context: context,
-                removeTop: true,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    Image.network(
-                      'https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGl2aW5ncm9vbSUyMGZ1cm5pdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
-                      width: 100.0,
-                      height: 200.0,
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: SizedBox(
+                height: 150,
+                width: double.infinity,
+                child: MediaQuery.removePadding(
+                  context: context,
+                  removeTop: true,
+                  child: SizedBox(
+                    height: 200,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        Image.network(
+                          'https://images.unsplash.com/photo-1505693416388-ac5ce068fe85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmVkfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+                          width: 150.0,
+                          height: 200.0,
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Image.network(
+                          'https://images.unsplash.com/photo-1617325247661-675ab4b64ae2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGJlZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+                          width: 150.0,
+                          height: 100.0,
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Image.network(
+                          'https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGl2aW5ncm9vbSUyMGZ1cm5pdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
+                          width: 150.0,
+                          height: 100.0,
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Image.network(
+                          'https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGl2aW5ncm9vbSUyMGZ1cm5pdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
+                          width: 100.0,
+                          height: 100.0,
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Image.network(
+                          'https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGl2aW5ncm9vbSUyMGZ1cm5pdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
+                          width: 100.0,
+                          height: 100.0,
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Image.network(
+                          'https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGl2aW5ncm9vbSUyMGZ1cm5pdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
+                          width: 100.0,
+                          height: 100.0,
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Image.network(
+                          'https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGl2aW5ncm9vbSUyMGZ1cm5pdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
+                          width: 100.0,
+                          height: 100.0,
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                      ],
                     ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Image.network(
-                      'https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGl2aW5ncm9vbSUyMGZ1cm5pdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
-                      width: 100.0,
-                      height: 100.0,
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Image.network(
-                      'https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGl2aW5ncm9vbSUyMGZ1cm5pdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
-                      width: 100.0,
-                      height: 100.0,
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Image.network(
-                      'https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGl2aW5ncm9vbSUyMGZ1cm5pdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
-                      width: 100.0,
-                      height: 100.0,
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Image.network(
-                      'https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGl2aW5ncm9vbSUyMGZ1cm5pdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
-                      width: 100.0,
-                      height: 100.0,
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Image.network(
-                      'https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGl2aW5ncm9vbSUyMGZ1cm5pdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
-                      width: 100.0,
-                      height: 100.0,
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Image.network(
-                      'https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGl2aW5ncm9vbSUyMGZ1cm5pdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
-                      width: 100.0,
-                      height: 100.0,
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                  ],
+                  ),
                 ),
               ),
             ),
